399092542339ac407a41f1d8c4c10599c56db3c2
hazbiin <hzbi717@gmail.com>
2025-06-12 14:05:29 +0530

    used RouterLink

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,7 @@
 <script setup>
     import jobData from '@/jobs.json';
     import { ref, defineProps } from 'vue';
+    import { RouterLink } from 'vue-router';
     import JobListing from './JobListing.vue';
 
     defineProps({
@@ -27,10 +28,10 @@
         </div>
     </section>
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 </template>
